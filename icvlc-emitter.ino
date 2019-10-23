5b26a8e307b3ad86e7a4c31ffacbafce4028aae6
Hanjun Kim <hallazzang@gmail.com>
2019-10-23 19:30:39 +0900

    시리얼 읽기

--- icvlc-emitter.ino
+++ icvlc-emitter.ino
@@ -1,15 +1,9 @@
-#include <SoftwareSerial.h>
-
-SoftwareSerial VLCSerial(3,2); 
-
 void setup() {
   Serial.begin(9600);
-  VLCSerial.begin(400);
-  VLCSerial.print('&');
 }
 
 void loop() {
-  Serial.print('A');
-  VLCSerial.print('A');
-  delay(1000);
+  if (Serial.available() > 0) {
+    int ch = Serial.read();
+  }
 }
