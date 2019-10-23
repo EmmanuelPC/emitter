fc86954f554f646cbe24940e42369d2d6a879ec0
Hanjun Kim <hallazzang@gmail.com>
2019-10-23 20:57:27 +0900

    심볼 전송 기능 구현

--- icvlc-emitter.ino
+++ icvlc-emitter.ino
@@ -1,19 +1,74 @@
+#include <TimerOne.h>
+#include "Manchester.h"
+#include "Channel.h"
+
+#define SYMBOL_PERIOD 10000
+#define NUM_SYMBOLS 20
+
+#define STX 0x02
+#define ETX 0x03
+
 #define LED_PIN 2
+
+Channel transmitCh, serialCh;
+
+unsigned long symbols = 0;
+int symbolCounter = 0;
+
+void timerInterrupt() {
+  if (symbols == 0 || symbolCounter == NUM_SYMBOLS) {
+    char ch;
+
+    if (transmitCh.get(&ch)) {
+      symbols = encodeWord(ch);
+    } else {
+      symbols = 0x55555;
+    }
+
+    symbolCounter = 0;
+  }
+
+  if (symbols & 0x80000) {
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    digitalWrite(LED_PIN, LOW);
+  }
+  symbols <<= 1;
+  symbolCounter++;
+}
 
 void setup() {
   Serial.begin(9600);
 
   pinMode(LED_PIN, OUTPUT);
+
+  Timer1.initialize(SYMBOL_PERIOD);
+  Timer1.attachInterrupt(timerInterrupt);
 }
 
 void loop() {
   if (Serial.available() > 0) {
     int ch = Serial.read();
 
-    if (ch == '0') {
-      digitalWrite(LED_PIN, LOW);
-    } else if (ch == '1') {
-      digitalWrite(LED_PIN, HIGH);
+    if (ch == '\n') {
+      char c;
+
+      while (!transmitCh.put(STX)) {
+        delay(1);
+      }
+      while (serialCh.get(&c)) {
+        while (!transmitCh.put(c)) {
+          delay(1);
+        }
+      }
+      while (!transmitCh.put(ETX)) {
+        delay(1);
+      }
+    } else {
+      if (!serialCh.put(ch)) {
+        serialCh.get();
+        serialCh.put(ch);
+      }
     }
   }
 }
