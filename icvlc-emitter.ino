ef0b445e236207742bc4d8812d4e08cbfb70a74b
Hanjun Kim <hallazzang@gmail.com>
2019-10-23 19:34:20 +0900

    시리얼 입력에 따라 LED 켜고 끄기

--- icvlc-emitter.ino
+++ icvlc-emitter.ino
@@ -1,9 +1,19 @@
+#define LED_PIN 2
+
 void setup() {
   Serial.begin(9600);
+
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
   if (Serial.available() > 0) {
     int ch = Serial.read();
+
+    if (ch == '0') {
+      digitalWrite(LED_PIN, LOW);
+    } else if (ch == '1') {
+      digitalWrite(LED_PIN, HIGH);
+    }
   }
 }
